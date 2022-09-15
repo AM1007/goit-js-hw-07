8f9f9977554559539a572f528ded24a7ebd1531a
zloykoreshok <daodzen45@gmail.com>
2022-09-15 13:20:18 +0300

    SimpleLightbox markup added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -26,7 +26,8 @@ new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   animationSpeed: 250,
   overlay: true,
-  close: false,
   showCounter: false,
   fadeSpeed: 200,
+  close: true,
+  showCounter: true,
 });
