1c8836a53880d989789786c9c3c78bbf6c60afcb
zloykoreshok <daodzen45@gmail.com>
2022-09-15 13:07:08 +0300

    some strings added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const rootGallery = document.querySelector(".gallery");
+const imageGallery = createGallery(galleryItems);
+rootGallery.insertAdjacentHTML("afterbegin", imageGallery);
+rootGallery.addEventListener("click", handleClickOnImage);
+
+function createGallery(arrOfObj) {
+  return arrOfObj
+    .map((elem) => {
+      const { preview, original, description } = elem;
+      return `
+    <div>
+        <img 
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}/>
+        </div>`;
+    })
+    .join("");
+}
+
+// click on image handler //
+
+function handleClickOnImage(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}", width = "640", height = "480"></div>`
+  );
+  instance.show();
+
+  // close on Escape v1//
+
+  window.addEventListener("keydown", onEscape);
+  function onEscape(evt) {
+    const ESC_KEY_CODE = "Escape";
+    if (evt.code === ESC_KEY_CODE) {
+      instance.close();
+      window.removeEventListener("keydown", onEscape);
+    }
+  }
+}
